1f943e9823a215831a208a8e7cc649d7104fd20a
Gabe-N-Olivas <gabriel.n.olivas@gmail.com>
2023-03-02 08:13:05 -0700

    rsl finished

--- lib/rsl.py
+++ lib/rsl.py
@@ -3,22 +3,22 @@ from sys import platform
 
 module_path = inspect.getfile(inspect.currentframe())
 module_dir = os.path.realpath(os.path.dirname(module_path))
-
-if platform == "win32" or  platform == "darwin":
-    raise OSError("RSL was built with Linux in mind.")
-
 config = configparser.ConfigParser()
 config.read(f'{module_dir}/rsl.cfg')
 
+if platform == "win32" and config.get("debug","FORCE_WIN_LOAD") == False:
+    raise OSError("RSL was built with POSIX in mind. You can attempt to run this by setting FORCE_LOAD_WIN to True")
+
+if __name__ == "__main__" and config.get("debug","FORCE_RUN_MAIN") == False: raise Exception("RSL is module.")
+
 def initiate(pwd):
-    os.makedirs(f"{pwd}/logs")
+    try: os.makedirs(f"{pwd}/logs")
+    except FileNotFoundError: pwd = config.get("save","global-save")
+    with open(f"{pwd}/logs/log.txt", 'a'):
+        os.utime(f"{pwd}/logs", None)
 
-def mklog():
+def mklog(pwd, input):
+    with open(f"{pwd}/logs/log.txt", mode='w', encoding='utf-8') as f:
+        f.write(input)
 
-    config["var"]["current-log-file"] = str(int(config["var"]["current-log-file"]) + 1)
-    with open(f'{module_dir}/rsl.cfg') as conf:
-        config.write(conf)
-    print(config["var"]["current-log-file"])
-
-mklog()
-
+print("RSL Loaded with no setup issues")
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os, inspect
 from lib import rsl
+
 module_path = inspect.getfile(inspect.currentframe())
 module_dir = os.path.realpath(os.path.dirname(module_path))
 
 os.chdir(module_dir)
 
 if not os.path.exists("logs"): rsl.initiate(module_dir)
+print(os.path.realpath(os.path.dirname(module_path)))
